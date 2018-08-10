3696c2acc4bbf135a4c3d8d24828c4e6cdd6aed0
Stas Sribnyi <stas.sribnyi@gmail.com>
2018-08-10 16:36:57 +0300

    Change markup

--- image-manager/src/pages/images/Images.js
+++ image-manager/src/pages/images/Images.js
@@ -4,6 +4,8 @@ import config from '../../assets/config';
 
 import './Images.css';
 
+const DEFAULT_PAGE_SIZE_OPTIONS = [1, 5, 25, 50];
+
 const instance = axios.create({
   baseURL: config.BASE_URL
 });
@@ -14,30 +16,55 @@ class Images extends Component {
 
     this.state = {
       images: []
-    }
+    };
   }
 
   componentDidMount() {
-    instance.get('/images')
-      .then(response => {
-        console.log('fetched');
-        
-        this.setState({ images: response.data })
-      });
+    instance.get('/images').then(response => {
+      console.log('fetched');
+
+      this.setState({ images: response.data });
+    });
   }
 
   render() {
-    const imgs = this.state.images.map(x => <img
-      src={x.url}
-      key={x.id}
-      height="100"
-      width="100" />);
+    const trs = this.state.images.map((image, i) => (
+      <tr key={image.id}>
+        <td>{i + 1}</td>
+        <td>
+          <a href={image.url}>
+            <span className="image" style={{ backgroundImage: `url("${image.url}")` }} />
+          </a>
+        </td>
+        <td>{image.tooltip}</td>
+      </tr>
+    ));
 
     return (
-      <div>
-        {
-          imgs
-        }
+      <div className="row">
+        <div className="col">
+          <div className="row">
+            <div className="col">
+              <div className="responsive-image-table">
+                <table className="table table-striped">
+                  <thead className="thead-dark">
+                    <tr>
+                      <th>#</th>
+                      <th>Url</th>
+                      <th>Tooltip</th>
+                    </tr>
+                  </thead>
+                  <tbody>{trs}</tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="mx-auto col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+              {/* pagination */}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
