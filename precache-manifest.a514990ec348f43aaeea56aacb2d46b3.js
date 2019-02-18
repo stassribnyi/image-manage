5590929d5a68cd0dd039dd551be3882e18e09f9d
Stanislav Sribnyi <stanislav.sribnyi@caspio.com>
2019-02-18 13:36:34 +0200

    Updates

---
+++ precache-manifest.a514990ec348f43aaeea56aacb2d46b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaa24f5ccf6e85c6c3cd9bac95973876",
+    "url": "/image-manager/static/media/logo.aaa24f5c.png"
+  },
+  {
+    "revision": "abb302b48a7794c3537ef5ed4b07947c",
+    "url": "/image-manager/static/media/image-placeholder.abb302b4.svg"
+  },
+  {
+    "revision": "d89c1aeaac8d1b3d9f62",
+    "url": "/image-manager/static/js/runtime~main.d89c1aea.js"
+  },
+  {
+    "revision": "8c532cb084b2242aa985",
+    "url": "/image-manager/static/js/main.8c532cb0.chunk.js"
+  },
+  {
+    "revision": "e746e8f427927cb3b766",
+    "url": "/image-manager/static/js/2.e746e8f4.chunk.js"
+  },
+  {
+    "revision": "8c532cb084b2242aa985",
+    "url": "/image-manager/static/css/main.4b50637b.chunk.css"
+  },
+  {
+    "revision": "5736e9a40600c411dd843b83d386c0fe",
+    "url": "/image-manager/index.html"
+  }
+];
