5066f1662a5f09bb634432ad297cdfdc41455765
Stas Sribnyi <stas.sribnyi@gmail.com>
2019-02-20 21:07:29 +0200

    Fix naming and refactor navabar component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 
-import logoPath from '../../assets/logo.png';
+import logoPath from '../../assets/home.png';
 
 import './Navbar.css';
+
+const NavItem = props => {
+  const iconClass = ['icon', props.icon].join(' ');
+
+  return (
+    <li className="nav-item">
+      <NavLink
+        to={props.to}
+        exact
+        className="nav-link"
+        activeClassName="active"
+        onClick={props.onClick}
+      >
+        <span className={iconClass} />
+        <span className="nav-link-name">{props.name}</span>
+      </NavLink>
+    </li>
+  );
+};
 
 export default class Navigation extends Component {
   constructor(props) {
@@ -25,10 +44,23 @@ export default class Navigation extends Component {
   render() {
     const navClass = this.state.collapsed ? 'collapse' : '';
 
+    const routes = [
+      {
+        to: '/',
+        icon: 'all-images',
+        name: 'All Images'
+      },
+      {
+        to: '/new',
+        icon: 'add-new-image',
+        name: 'Add new image'
+      }
+    ];
+
     return (
       <nav className="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
         <NavLink to="/" className="navbar-brand" onClick={this.toggleCollapse}>
-          <img src={logoPath} alt="The manager logo" className="logo" />
+          <span className="icon home" />
         </NavLink>
         <button
           className="navbar-toggler"
@@ -43,27 +75,15 @@ export default class Navigation extends Component {
 
         <div className={`navbar-collapse ${navClass}`}>
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <NavLink
-                to="/"
-                exact
-                className="nav-link"
-                activeClassName="active"
+            {routes.map((route, i) => (
+              <NavItem
+                key={i}
+                to={route.to}
+                icon={route.icon}
+                name={route.name}
                 onClick={this.toggleCollapse}
-              >
-                All Images
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/new"
-                className="nav-link"
-                activeClassName="active"
-                onClick={this.toggleCollapse}
-              >
-                Add new image
-              </NavLink>
-            </li>
+              />
+            ))}
           </ul>
         </div>
       </nav>
